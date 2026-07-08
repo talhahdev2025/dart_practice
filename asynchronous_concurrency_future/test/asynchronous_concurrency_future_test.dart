6abe92cf4a00b7408a5aae606f74e483d0c2bb4b
Talha <talhahdev2025@gmail.com>
2026-07-08 14:24:20 +0500

    setup project to practice dart

---
+++ asynchronous_concurrency_future/test/asynchronous_concurrency_future_test.dart
@@ -0,0 +1,8 @@
+import 'package:asynchronous_concurrency_future/asynchronous_concurrency_future.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
